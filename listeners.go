aedabf73bc4e81e9115542d1844f3f9c93b1e426
Michał Kubik <bopke2@gmail.com>
2020-06-20 12:57:12 +0200

    Remove "gang witaczy" role ping from user join message

--- listeners.go
+++ listeners.go
@@ -170,7 +170,7 @@ func OnMessageReactionAdd(s *discordgo.Session, reaction *discordgo.MessageReact
 			URL: "https://media1.tenor.com/images/b7ac38f04efc899c84ff9975132f4add/tenor.gif",
 		},
 	}
-	content := member.Mention() + " | <@&660867726037745665>"
+	content := member.Mention()
 	if !wasAbleToAddAllRoles {
 		content += "\n Nie udało mi się nadać Ci wszystkich ról :worried:"
 	}
